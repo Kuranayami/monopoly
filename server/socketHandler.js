6a1c85ef97051b56faa8239ef23f0ccdc013fcb4
ayenn devera <ayenndevera@gmail.com>
2026-06-22 20:02:34 +0800

    Filter 0-player rooms from listing

--- server/socketHandler.js
+++ server/socketHandler.js
@@ -753,7 +753,7 @@ export default function socketHandler(io) {
     socket.on('list_rooms', async (callback) => {
       try {
         const games = await listGames();
-        const rooms = games.filter(g => g.status === 'waiting').map(g => ({
+        const rooms = games.filter(g => g.status === 'waiting' && g.players.length > 0).map(g => ({
           roomCode: g.roomCode,
           hostName: g.hostName,
           playerCount: g.players.length,
